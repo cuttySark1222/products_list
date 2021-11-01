804bdbab39babdb0fb085ad0e45e8606e01572bb
Larry Zhou <webgrowbig@gmail.com>
2021-11-01 16:37:39 -0400

    Update Python file

--- products_list.py
+++ products_list.py
@@ -1,14 +1,18 @@
+import os
+
 products = [] # 建立购物清单
 
-# 增加读取购物清单的需求
-with open('products-list.csv', 'r', encoding='utf-8') as f:
-	for line in f:
-		if '商品,价格' in line:
-			continue
-		name, price = line.strip().split(',')
-		products.append([name, price])
-print(products)
+# 读取档案
+if os.path.isfile('products-list.csv'): # 检查档案是否存在
+	with open('products-list.csv', 'r', encoding='utf-8') as f:
+		for line in f:
+			if '商品,价格' in line:
+				continue
+			name, price = line.strip().split(',')
+			products.append([name, price])
+	print(products)
 
+# 使用者输入
 while True:
 	name = input("请输入商品名称： ")
 	if name == 'q': # 如果使用者输入 q ，程序终止
@@ -16,6 +20,7 @@ while True:
 	price = input("请输入商品价格： ")
 	products.append([name, price]) # 购物清单添加 [商品，价格] 的二级清单
 
+# 打印商品，计算总价格
 total = 0 
 product = []
 for p in products:
@@ -25,6 +30,7 @@ for p in products:
 print(f'本次购物购买了： {product}')	
 print(f'本次购物总花费：{total}')
 
+# 写入档案
 with open('products-list.csv', 'w', encoding='utf-8') as f:
 	f.write('商品' + ',' + '价格' + '\n')
 	for d in products:
